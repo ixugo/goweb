f4e22147eda638b28b0e606e9476d77c73b78066
xugo <xx@golang.space>
2025-02-18 11:08:47 +0800

    add ttl cache

--- pkg/hook/cache.go
+++ pkg/hook/cache.go
@@ -1,5 +1,11 @@
 package hook
 
+import (
+	"time"
+
+	"github.com/ixugo/goweb/pkg/conc"
+)
+
 // UseCache 使用内存缓存一些临时数据
 // 由于是临时数据，其 value 应该是一次性数据，用完即丢
 // 第二个返回参数用来标识是否命中缓存
@@ -13,6 +19,23 @@ func UseCache[K comparable, V any](fn func(K) (V, error)) func(K) (V, bool, erro
 		v, err := fn(key)
 		if err == nil {
 			cache[key] = v
+		}
+		return v, false, err
+	}
+}
+
+// UseTTLCache 使用内存缓存一些临时数据
+// 指定时间超时自动清理
+func UseTTLCache[K comparable, V any](timeline time.Duration, fn func(K) (V, error)) func(K) (V, bool, error) {
+	cache := conc.NewTTLMap[K, V]()
+	return func(key K) (V, bool, error) {
+		v, ok := cache.Load(key)
+		if ok {
+			return v, true, nil
+		}
+		v, err := fn(key)
+		if err == nil {
+			cache.Store(key, v, timeline)
 		}
 		return v, false, err
 	}
